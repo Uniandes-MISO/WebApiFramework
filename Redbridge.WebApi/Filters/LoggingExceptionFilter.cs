fe85b84bdad5d6b3e62a2e92b0b54d5d807f388e
wilson-bg <w.beltran@uniandes.edu.co>
2022-11-06 16:52:09 -0500

    update

---
+++ Redbridge.WebApi/Filters/LoggingExceptionFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Filters;
+using Redbridge.Diagnostics;
+
+namespace Redbridge.WebApi.Filters
+{
+    public class LoggingExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger _logger;
+
+        public LoggingExceptionFilter(ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Exception != null)
+            {
+                _logger.WriteInfo($"Logging that an exception has occurred in LoggingExceptionFilter: {actionExecutedContext.Exception.Message}...");
+                var messagePhrase = actionExecutedContext.Exception.Message ?? "Internal server error - no additional detail supplied";
+                messagePhrase = string.Join(",", messagePhrase.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Where(s => !string.IsNullOrWhiteSpace(s))); // Carriage returns are not permitted in reason phrases.
+
+                var response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    RequestMessage = actionExecutedContext.Request,
+                    ReasonPhrase = messagePhrase
+                };
+
+                actionExecutedContext.Response = response;
+                _logger.WriteException(actionExecutedContext.Exception);
+            }
+        }
+    }
+}
